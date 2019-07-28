42a0c2854e8ec74ead3ef98ba9065d8fd59a9f3c
Raghi Ramachandran <raghir01@gmail.com>
2019-07-28 11:33:11 -0400

    Initial project structure created

---
+++ group5-config-server/src/main/java/com/company/group5configserver/Group5ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.group5configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group5ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group5ConfigServerApplication.class, args);
+	}
+
+}
---
+++ group5-eureka-service-registry/src/main/java/com/company/group5eurekaserviceregistry/Group5EurekaServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.company.group5eurekaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group5EurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group5EurekaServiceRegistryApplication.class, args);
+	}
+
+}
