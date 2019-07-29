36efcda178cbee72da97e49cd7f43aef778f458c
Nurul Alam <nurul.alam1@hotmail.com>
2019-07-29 10:29:18 -0400

    note model

--- note-service/src/main/java/com/company/noteservice/dao/NoteDaoJdbcTemplateImpl.java
+++ note-service/src/main/java/com/company/noteservice/dao/NoteDaoJdbcTemplateImpl.java
@@ -2,10 +2,24 @@ package com.company.noteservice.dao;
 
 
 import com.company.noteservice.model.Note;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class NoteDaoJdbcTemplateImpl implements NoteDao{
+
+    public static final String INSERT_NOTE =
+            "seletc int note (note_id, book_id, note)values (?, ?, ?)";
+    public static final String SELECT_NOTE_BY_ID =
+            "";
+    public static final String SELECT_ALL_NOTES =
+            "";
+    public static final String UPDATE_NOTE =
+            "";
+    public static final String DELETE_NOTE =
+            "";
+
     @Override
     public Note getNote(int note_id) {
         return null;
@@ -25,6 +39,7 @@ public class NoteDaoJdbcTemplateImpl implements NoteDao{
     public void deleteNote(int note_id) {
 
     }
+
 
     @Override
     public void updateNote(Note note) {
