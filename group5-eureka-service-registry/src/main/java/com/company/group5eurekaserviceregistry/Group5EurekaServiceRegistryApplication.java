d66ccb02076cd4f6bf687cc58e7b356a28e7a82a
Raghi Ramachandran <raghir01@gmail.com>
2019-07-28 22:56:35 -0400

    eureka service registry created

--- group5-eureka-service-registry/src/main/java/com/company/group5eurekaserviceregistry/Group5EurekaServiceRegistryApplication.java
+++ group5-eureka-service-registry/src/main/java/com/company/group5eurekaserviceregistry/Group5EurekaServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.company.group5eurekaserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Group5EurekaServiceRegistryApplication {
 
 	public static void main(String[] args) {
