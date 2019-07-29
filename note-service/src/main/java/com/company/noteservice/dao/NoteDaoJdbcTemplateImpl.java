5c9ceb12640e9cf7de73d8e0d613599b01a9ec1c
Raghi Ramachandran <raghir01@gmail.com>
2019-07-29 11:28:56 -0400

    note-service dao, daoimpl and daotest

--- note-service/src/main/java/com/company/noteservice/dao/NoteDaoJdbcTemplateImpl.java
+++ note-service/src/main/java/com/company/noteservice/dao/NoteDaoJdbcTemplateImpl.java
@@ -2,32 +2,91 @@ package com.company.noteservice.dao;
 
 
 import com.company.noteservice.model.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class NoteDaoJdbcTemplateImpl implements NoteDao{
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public NoteDaoJdbcTemplateImpl(JdbcTemplate newjdbcTemplate){
+        this.jdbcTemplate = newjdbcTemplate;
+    }
+
+    //Prepared Statement
+    private static final String INSERT_NOTE_SQL =
+            "insert into note (book_id, note) values (?,?)";
+
+    private static final String SELECT_NOTE_SQL =
+            "select * from note where note_id = ?";
+
+    private static final String SELECT_ALL_NOTE_SQL =
+            "select * from note";
+
+    private static final String DELETE_NOTE_SQL =
+            "delete from note where note_id = ?";
+
+    private static final String UPDATE_NOTE_SQL =
+            "update note set book_id = ?, note = ? where note_id = ?";
+
+    private Note mapRowToNote(ResultSet rs, int rowNum) throws SQLException {
+        Note note = new Note();
+        note.setNoteId(rs.getInt("note_id"));
+        note.setBookId(rs.getInt("book_id"));
+        note.setNote(rs.getString("note"));
+
+        return note;
+    }
+
     @Override
-    public Note getNote(int note_id) {
-        return null;
+    public Note getNote(int noteId) {
+        try {
+            return jdbcTemplate.queryForObject(SELECT_NOTE_SQL, this::mapRowToNote, noteId);
+        } catch (EmptyResultDataAccessException e){
+            // if there is no match for this note id, return null
+            return null;
+        }
     }
 
     @Override
     public List<Note> getAllNotes() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_NOTE_SQL, this::mapRowToNote);
     }
 
     @Override
     public Note addNote(Note note) {
-        return null;
+        jdbcTemplate.update(
+                INSERT_NOTE_SQL,
+                note.getBookId(),
+                note.getNote());
+        int id = jdbcTemplate.queryForObject("select last_insert_id()",Integer.class);
+        note.setNoteId(id);
+        return note;
     }
 
     @Override
-    public void deleteNote(int note_id) {
+    public void deleteNote(int noteId) {
+        jdbcTemplate.update(DELETE_NOTE_SQL,noteId);
 
     }
 
     @Override
     public void updateNote(Note note) {
-
+        jdbcTemplate.update(
+                UPDATE_NOTE_SQL,
+                note.getBookId(),
+                note.getNote(),
+                note.getNoteId());
     }
+
+
+
 }
--- note-service/src/test/java/com/company/noteservice/dao/NoteDaoTest.java
+++ note-service/src/test/java/com/company/noteservice/dao/NoteDaoTest.java
@@ -1,28 +1,100 @@
 package com.company.noteservice.dao;
 
+import com.company.noteservice.model.Note;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class NoteDaoTest {
+
+    @Autowired
+    NoteDao noteDao;
+
+    @Before
+    public void setUp() throws Exception {
+        List<Note> noteList = noteDao.getAllNotes();
+        for(Note note : noteList){
+            noteDao.deleteNote(note.getNoteId());
+        }
+    }
 
     @Test
     public void getNote() {
+        Note note1 = new Note();
+        note1.setBookId(1);
+        note1.setNote("my notes");
+        note1 = noteDao.addNote(note1);
+
+        Note note2 = new Note();
+        note2.setBookId(1);
+        note2.setNote("my notes");
+        note2 = noteDao.addNote(note2);
+
+        Note note3 = noteDao.getNote(note1.getNoteId());
+        assertEquals(note1, note3);
+        note3 = noteDao.getNote(note2.getNoteId());
+        assertEquals(note2, note3);
     }
 
     @Test
     public void getAllNotes() {
+        Note note1 = new Note();
+        note1.setBookId(1);
+        note1.setNote("my notes");
+        note1 = noteDao.addNote(note1);
+
+        Note note2 = new Note();
+        note2.setBookId(1);
+        note2.setNote("my notes");
+        note2 = noteDao.addNote(note2);
+
+        List<Note> noteList = noteDao.getAllNotes();
+        assertEquals(noteList.size(),2);
     }
 
     @Test
     public void addNote() {
+        Note note = new Note();
+        note.setBookId(1);
+        note.setNote("my notes");
+        note = noteDao.addNote(note);
+
+        Note note1 = noteDao.getNote(note.getNoteId());
+        assertEquals(note1, note);
     }
 
     @Test
     public void deleteNote() {
+        Note note = new Note();
+        note.setBookId(1);
+        note.setNote("my notes");
+        note = noteDao.addNote(note);
+
+        noteDao.deleteNote(note.getNoteId());
+        Note note1 = noteDao.getNote(note.getNoteId());
+        assertNull(note1);
     }
 
     @Test
     public void updateNote() {
+        Note note = new Note();
+        note.setBookId(1);
+        note.setNote("my notes");
+        note = noteDao.addNote(note);
+
+        note.setBookId(10);
+        noteDao.updateNote(note);
+
+        Note note1 = noteDao.getNote(note.getNoteId());
+        assertEquals(note1, note);
     }
 }
